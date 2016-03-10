8fb138d302c919a57bac59912a723c96032b5ed2
Jeremy Crafts <jcrafts@redhat.com>
2016-03-10 09:56:17 -0500

    ! to not

--- redhat_access_insights/__init__.py
+++ redhat_access_insights/__init__.py
@@ -528,7 +528,7 @@ def handle_startup(options, config):
         reg_check, status = registration_check(config)
         logger.info('\n'.join(reg_check))
         # exit with !status, 0 for True, 1 for False
-        sys.exit(!status)
+        sys.exit(not status)
 
     # Set offline mode for OSP use
     offline_mode = False
