da055a67abd9bb2f61a6d7bd4c78db13bb60da98
Richard Brantley <brantleyr@gmail.com>
2017-05-01 12:08:30 -0400

    bump the build

--- insights_client/constants.py
+++ insights_client/constants.py
@@ -6,7 +6,7 @@ import os
 
 class InsightsConstants(object):
     app_name = 'insights-client'
-    version = '2.0.5-8'
+    version = '2.0.5-9'
     auth_method = 'BASIC'
     log_level = 'DEBUG'
     package_path = os.path.dirname(
